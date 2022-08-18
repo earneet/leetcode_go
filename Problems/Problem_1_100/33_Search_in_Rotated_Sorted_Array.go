2531efa0e209f92707c30aab81e1fcfc86b92839
earneet <earneet@gmail.com>
2022-08-18 15:49:20 +0800

    2022/08/18 daily problems

---
+++ Problems/Problem_1_100/33_Search_in_Rotated_Sorted_Array.go
@@ -0,0 +1,25 @@
+package Problem_1_100
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[left] {
+			if nums[mid] >= target && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] <= target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
