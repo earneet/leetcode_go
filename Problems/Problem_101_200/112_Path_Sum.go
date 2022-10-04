cf621b677f63f2c90b2243396e84a8fb268f0439
earneet <earneet@gmail.com>
2022-10-04 09:19:50 +0800

    2022/10/04 daily problems

---
+++ Problems/Problem_101_200/112_Path_Sum.go
@@ -0,0 +1,13 @@
+package Problem_101_200
+
+import . "LeetCode_Go/DataStructure"
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil && root.Val == targetSum {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
